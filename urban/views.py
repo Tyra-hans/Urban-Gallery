4f47a2c06734c0f4893dda74b1af03596817ee37
Tyra-hans <tyrahans17@gmail.com>
2019-11-10 23:26:55 +0300

    builds on base.html to allow for js to be used

--- urban/views.py
+++ urban/views.py
@@ -7,30 +7,12 @@ from django.http  import HttpResponse
 def welcome(request):
     return render(request, 'welcome.html')
 
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
 def photos_of_day(request):
     date = dt.date.today()
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+
+    return render(request, 'all-photos/todays-photos.html', {"date": date,})
 
 def past_days_photos(request,past_date):
         # Converts data from the string Url
--- urbangallery/settings.py
+++ urbangallery/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'urban.apps.UrbanConfig',
+    'urban',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
